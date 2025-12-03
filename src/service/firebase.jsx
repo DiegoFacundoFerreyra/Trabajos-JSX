1f79a356a7f414cc959fc025778b11ce7654aa53
Diego Ferreyra <diegofacundoferreyra@gmail.com>
2025-12-02 22:29:11 -0300

    Se complico con los archivos del carrito

---
+++ src/service/firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5_ZFRE2DJVn0e3yb1do2P20DAjZ8R0uY",
+  authDomain: "primer-trabajo-practico-diego.firebaseapp.com",
+  projectId: "primer-trabajo-practico-diego",
+  storageBucket: "primer-trabajo-practico-diego.firebasestorage.app",
+  messagingSenderId: "109370492978",
+  appId: "1:109370492978:web:b97c5cb2a1c19da1ea942b",
+  measurementId: "G-KGHZXQTB5J",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
